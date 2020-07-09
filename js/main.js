80e9fa460624349181da7b29f456215e4397c8c4
squarlll <lilsquarl@gmail.com>
2020-07-09 21:42:39 +0300

    fsad

--- js/main.js
+++ js/main.js
@@ -123,7 +123,7 @@ $(document).on("scroll", function(){
 	} else{
 		$('.scroll__button').removeClass('active');
 	}
-	if($(document).scrollTop()>5000){
+	if($(document).scrollTop()>8000){
 		$('.call__button-wrapper').addClass('hidden');
 	} else{
 		$('.call__button-wrapper').removeClass('hidden');

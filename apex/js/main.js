4f143a69305a2e37b71e7544829959ecd3a139d8
squarlll <lilsquarl@gmail.com>
2020-07-16 13:12:46 +0300

    fsad

---
+++ apex/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('img, a').on('dragstart', function(event) { event.preventDefault(); });
+	$('.item__info').slideUp(0);
+	$('.item__content').on('click', function(){
+		$(this).siblings('.item__info').slideToggle(300);
+		$(this).parent().siblings().children('.item__info').slideUp(300);
+		$(this).toggleClass('active');
+		$(this).parent().siblings().children('.item__content').removeClass('active');
+	});
+});

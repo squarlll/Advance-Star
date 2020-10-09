df22f62df76f73f8df80c25fd40b02743f6a66d9
squarlll <lilsquarl@gmail.com>
2020-10-09 22:00:00 +0300

    уввв

--- electrical-installation/js/main.js
+++ electrical-installation/js/main.js
@@ -5,4 +5,21 @@ $(document).ready(function(){
 		$('.work__main-image img').prop('src', workImagesItem);
 		$('.work__main-image source').prop('srcset', workImagesItem);
 	});
+
+	$('.header__dropdown').on('mouseenter', function(){
+		$(this).children('.header__dropdown-list').addClass('_visible');
+		$(this).on('mouseleave', function(){
+			$(this).children('.header__dropdown-list').removeClass('_visible');
+		});
+	});
+
+	$('.burger-wrapper').on('click', function(){
+		$(this).toggleClass('_active');
+		$('.header__mobile-menu').addClass('_active');
+		$('body').addClass('_lock');
+		$('.mobile-menu__close').on('click', function(){
+			$('.header__mobile-menu').removeClass('_active');
+			$('body').removeClass('_lock');
+		});
+	});
 });

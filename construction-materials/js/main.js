2506cde1ad16d0bedf2d5c6b7118cf8c2e2836c1
squarlll <lilsquarl@gmail.com>
2020-10-14 21:52:12 +0300

    dasd

--- construction-materials/js/main.js
+++ construction-materials/js/main.js
@@ -1,7 +1,24 @@
 $(document).ready(function(){
-	$('.slider__images-item img').on('click', function(){
-		var clickedImage = $(this).attr('src');
-		$('.slider__main-image img').attr('src', clickedImage);
-		$('.slider__main-image source').attr('srcset', clickedImage);
+	$('.work__images-item img').on('click', function(){
+		var workImagesItem = $(this).attr('src');
+		$(this).parents('.work__images').siblings('.work__main-image').children('picture').children('img').prop('src', workImagesItem);
+		$(this).parents('.work__images').siblings('.work__main-image').children('picture').children('source').prop('srcset', workImagesItem);
+	});
+
+	$('.header__dropdown').on('mouseenter', function(){
+		$(this).children('.header__dropdown-list').addClass('_visible');
+		$(this).on('mouseleave', function(){
+			$(this).children('.header__dropdown-list').removeClass('_visible');
+		});
+	});
+
+	$('.burger-wrapper').on('click', function(){
+		$(this).toggleClass('_active');
+		$('.header__mobile-menu').addClass('_active');
+		$('body').addClass('_lock');
+		$('.mobile-menu__close').on('click', function(){
+			$('.header__mobile-menu').removeClass('_active');
+			$('body').removeClass('_lock');
+		});
 	});
 });

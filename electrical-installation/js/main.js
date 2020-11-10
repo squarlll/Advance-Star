791cff5cf30dd265aaa7f08d8722440a515fe8c5
squarlll <lilsquarl@gmail.com>
2020-11-10 19:37:30 +0300

    123

--- electrical-installation/js/main.js
+++ electrical-installation/js/main.js
@@ -1,4 +1,38 @@
 $(document).ready(function(){
+	const	menuNavigation = $('.header__navigation');
+	const menuNavigationOffset = $('.header__navigation').offset().top;
+	var	menuNavigationOffset1 = menuNavigationOffset - $('html').scrollTop();
+	if (menuNavigationOffset1 <= 10) {
+		menuNavigation.addClass('_sticky');
+		$('.header-wrapper').addClass('_sticky-wrapper');
+	} else if (menuNavigationOffset1 > 10) {
+		menuNavigation.removeClass('_sticky');
+		$('.header-wrapper').removeClass('_sticky-wrapper');
+	}
+	$(window).on('scroll', function(){
+		const	menuNavigation = $('.header__navigation');
+		var	menuNavigationOffset1 = menuNavigationOffset - $('html').scrollTop();
+		if (menuNavigationOffset1 <= 10) {
+			menuNavigation.addClass('_sticky');
+			$('.header-wrapper').addClass('_sticky-wrapper');
+		} else if (menuNavigationOffset1 > 10) {
+			menuNavigation.removeClass('_sticky');
+			$('.header-wrapper').removeClass('_sticky-wrapper');
+		}
+	});
+	$(window).on('resize', function(){
+		$(window).on('scroll', function(){
+			const	menuNavigation = $('.header__navigation');
+			var	menuNavigationOffset1 = menuNavigationOffset - $('html').scrollTop();
+			if (menuNavigationOffset1 <= 10) {
+				menuNavigation.addClass('_sticky');
+				$('.header-wrapper').addClass('_sticky-wrapper');
+			} else if (menuNavigationOffset1 > 10) {
+				menuNavigation.removeClass('_sticky');
+				$('.header-wrapper').removeClass('_sticky-wrapper');
+			}
+		});
+	});
 	// Модальные окна
 	$('.header__callback-button, .footer__callback-button').on('click', function(){
 		$('.popup__wrapper-call').addClass('_active');
@@ -10,12 +44,10 @@ $(document).ready(function(){
 		$(this).parents('.popup__wrapper').removeClass('_active');
 	});
 
-	$('.work__images-item img').on('click', function(){
-		var workImagesItem = $(this).attr('src');
-		$(this).parents('.work__images').siblings('.work__main-image').children('picture').children('img').prop('src', workImagesItem);
-		$(this).parents('.work__images').siblings('.work__main-image').children('picture').children('source').prop('srcset', workImagesItem);
-	});
+	// Маска на поля
+	$('.form__field').mask('+7 (999) 999-99-99');
 
+	// Дропдаун
 	$('.header__dropdown').on('mouseenter', function(){
 		$(this).children('.header__dropdown-list').addClass('_visible');
 		$(this).on('mouseleave', function(){
@@ -28,24 +60,41 @@ $(document).ready(function(){
 		$(this).toggleClass('_active');
 		$('.header__mobile-menu').addClass('_active');
 		$('body').addClass('_lock');
-		$('.mobile-menu__close').on('click', function(){
+		$('.mobile-menu__close, .mobile-menu__link').on('click', function(){
 			$('.header__mobile-menu').removeClass('_active');
 			$('body').removeClass('_lock');
 		});
 	});
 
+	// Плавная прокрутка
+	$('a[href^="#"]').click(function(){
+		var target = $(this).attr('href');
+		$('html, body').animate({scrollTop: $(target).offset().top - 50}, 1000);
+		return false;
+	});
+
+	// Прикрепление файла
 	$('.form__file-input').on('change', function(){
 		$('.form__file-text').text('Файл прикреплен')
 	});
 
-	// Плавная прокрутка
-	$('a[href^="#"]').click(function(){
-		var target = $(this).attr('href');
-		$('html, body').animate({scrollTop: $(target).offset().top - 100}, 1000);
-		return false;
+	// Переключение картинок в блоке с работами
+	$('.work__images-item img').on('click', function(){
+		var workImagesItem = $(this).attr('src');
+		$(this).parents('.work__images-item').addClass('_active');
+		$(this).parents('.work__images-item').siblings().removeClass('_active');
+		$(this).parents('.work__images').siblings('.work__main-image').children('picture').children('img').prop('src', workImagesItem);
+		$(this).parents('.work__images').siblings('.work__main-image').children('picture').children('source').prop('srcset', workImagesItem);
 	});
 
-	$('.form__field').mask('+7 (999) 999-99-99');
+	// Добавление сайта в избранное
+	$('.footer__favorites-button').on('click', function(){
+		alert('Нажмите Ctrl+D чтобы добавить страницу в закладки')
+	});
+
+	$('.text__button').on('click', function(){
+		$('.text-wrapper .text').toggleClass('_hidden');
+	});
 
 	// input range
 	$('.quiz__range').on('change', function(){
@@ -69,7 +118,6 @@ $(document).ready(function(){
 			url: "mail.php", //Change
 			data: th.serialize()
 		}).done(function() {
-			alert("Thank you!");
 			setTimeout(function() {
 				// Done Functions
 				th.trigger("reset");

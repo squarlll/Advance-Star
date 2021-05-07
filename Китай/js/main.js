6994482664e8b7acb04e60edf343f2bb470e5302
squarlll <55456306+squarlll@users.noreply.github.com>
2021-05-07 18:11:27 +0300

    Add files via upload

--- Китай/js/main.js
+++ Китай/js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-	
+	$('.popup_button').on('click', function(){
+		$('.popup__wrapper').addClass('_active');
+		$('html, body').addClass('_lock');
+	});
+	$('.popup__close, .popup__area').on('click', function(){
+		$('.popup__wrapper').removeClass('_active');
+		$('html, body').removeClass('_lock');
+	});
 });
